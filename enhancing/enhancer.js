0f7bed3ad2633435f814c66de4e1c39b28ddd820
johnkirtley <kirtleyj16@gmail.com>
2020-03-25 15:37:46 -0700

    succeed, fail, repair, get functions added

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,42 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+	succeed,
+	fail,
+	repair,
+	get
 };
 
 function succeed(item) {
-  return { ...item };
+	let { enhancement } = item;
+
+	if (enhancement === 20) {
+		return { ...item };
+	} else {
+		enhancement = enhancement + 1;
+		return { ...item, enhancement };
+	}
 }
 
 function fail(item) {
-  return { ...item };
+	let { enhancement } = item;
+	let { durability } = item;
+
+	if (enhancement < 15) {
+		return { ...item, durability: durability - 5 };
+	} else if (enhancement >= 15 && enhancement > 16) {
+		return {
+			...item,
+			durability: durability - 10,
+			enhancement: enhancement - 1
+		};
+	} else {
+		return { ...item, durability: durability - 10 };
+	}
 }
 
 function repair(item) {
-  return { ...item };
+	return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,11 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require('./enhancer.js');
 // test away!
+const item = {
+	name: 'string',
+	durability: 90,
+	enhancement: 12
+};
+
+it('repairing item should increase durability to 100', function() {
+	expect(repair(item)).toStrictEqual({ ...item, durability: 100 });
+});
